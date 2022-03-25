4c3a09cf080c2cf7d80c137ab25a43588b8fe24c
NatPstrag <nati.pstrag@gmail.com>
2022-03-25 14:25:28 +0100

    Initial commit

---
+++ src/main/java/com/example/onlinerecipebook/OnlineRecipeBookApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlinerecipebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineRecipeBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineRecipeBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/onlinerecipebook/OnlineRecipeBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlinerecipebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineRecipeBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
