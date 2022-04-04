10d86cc699466ef8be1b632224d395e6effe3e8b
NatPstrag <nati.pstrag@gmail.com>
2022-04-04 16:28:05 +0200

    Add cuisine into table "Cuisine"

--- src/main/java/com/example/onlinerecipebook/Cuisine/CuisineConfig.java
+++ src/main/java/com/example/onlinerecipebook/Cuisine/CuisineConfig.java
@@ -16,16 +16,16 @@ public class CuisineConfig {
         return args -> {
 
             Cuisine polish = new Cuisine(1, "Polish");
-            Cuisine french = new Cuisine(1, "French");
-            Cuisine spanish = new Cuisine(1, "Spanish");
-            Cuisine greek = new Cuisine(1, "Greek");
-            Cuisine japanese = new Cuisine(1, "Japanese");
-            Cuisine chinese = new Cuisine(1, "Chinese");
-            Cuisine fast_food = new Cuisine(1, "Fast-food");
-            Cuisine mexican = new Cuisine(1, "Mexican");
-            Cuisine indian = new Cuisine(1, "Indian");
-            Cuisine thai = new Cuisine(1, "Thai");
-            Cuisine italian = new Cuisine(1, "Italian");
+            Cuisine french = new Cuisine(2, "French");
+            Cuisine spanish = new Cuisine(3, "Spanish");
+            Cuisine greek = new Cuisine(4, "Greek");
+            Cuisine japanese = new Cuisine(5, "Japanese");
+            Cuisine chinese = new Cuisine(6, "Chinese");
+            Cuisine fast_food = new Cuisine(7, "Fast-food");
+            Cuisine mexican = new Cuisine(8, "Mexican");
+            Cuisine indian = new Cuisine(9, "Indian");
+            Cuisine thai = new Cuisine(10, "Thai");
+            Cuisine italian = new Cuisine(11, "Italian");
 
             List<Cuisine> cuisine = List.of(polish, french, spanish, greek, japanese, chinese, fast_food, mexican, indian, thai, italian);
             for(Cuisine x : cuisine){
