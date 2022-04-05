520e80ba95270e47a13a18e4dff24e6247db474a
NatPstrag <nati.pstrag@gmail.com>
2022-04-05 18:46:03 +0200

    Working on GET, PUT, POST and DELETE

--- src/main/java/com/example/onlinerecipebook/Recipe/RecipeController.java
+++ src/main/java/com/example/onlinerecipebook/Recipe/RecipeController.java
@@ -1,4 +1,37 @@
 package com.example.onlinerecipebook.Recipe;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+
+@Controller
 public class RecipeController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "start";
+    }
+
+
+    @GetMapping("/recipes")
+    public ResponseEntity fetchAllRecipes() {
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @GetMapping("/recipes/{id}")
+    public ResponseEntity fetchRecipesByID(@PathVariable("id") String id) {
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/recipes", consumes = "application/jason", produces = "application/json")
+    public Recipe createRecipe(@RequestBody Recipe recipe){
+        return recipe;
+    }
+
+    @DeleteMapping("/recipe/{id}/")
+    public ResponseEntity deleteRecipe(@PathVariable("id") String id){
+        return new ResponseEntity(HttpStatus.OK);
+    }
 }
